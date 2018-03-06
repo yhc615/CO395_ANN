30ad586a768f77072d78d403250079a253a79f76
Tom Miller <M1LL3RB0Y@users.noreply.github.com>
2018-03-06 00:40:45 +0000

    config changes

--- src/overfit_fcnet.py
+++ src/overfit_fcnet.py
@@ -10,22 +10,22 @@ TODO: Overfit the network with 50 samples of CIFAR-10
 ###########################################################################
 #                           BEGIN OF YOUR CODE                            #
 ###########################################################################
-out = get_CIFAR10_data(num_training=40,num_validation=10, num_test=0)
+out = get_CIFAR10_data(num_training=50,num_validation=10, num_test=0)
 data = {
       'X_train': out['X_train'], # training data
       'y_train': out['y_train'], # training labels
       'X_val':  out['X_val'], # validation data
       'y_val': out['y_val'] # validation labels
     }
-model = FullyConnectedNet(hidden_dims=[10 for i in range(3)], num_classes=10)
+model = FullyConnectedNet(hidden_dims=[100 for i in range(1)], num_classes=10)
 solver = Solver(model, data,
                 update_rule='sgd',
                 optim_config={
-                  'learning_rate': 1e-3,
+                  'learning_rate': 15e-4,
                 },
                 lr_decay=0.95,
-                num_epochs=10, batch_size=100,
-                print_every=100)
+                num_epochs=20, batch_size=100,
+                print_every=5)
 solver.train()
 
 ##############################################################################
