softmax fix

src/classifiers.py
@@ -25,12 +25,13 @@ def softmax(logits, y):
     ###########################################################################
     #                           BEGIN OF YOUR CODE                            #
     ###########################################################################
-    dlogits = np.exp(logits - np.max(logits))  #softmax numerator = e^(logit[j] + log(K)) where log(K) = -max(logits)
+    probs = np.exp(logits - np.max(logits, axis=1, keepdims=True))  #softmax numerator = e^(logit[j] + log(K)) where log(K) = -max(logits)
     N = logits.shape[0]     # N = number of samples
-    dlogits /= np.sum(dlogits, axis=1).reshape(N,-1)  #softmax denom = sum(e^(logit[j] + log(K))) where we sum for each example (row)
+    probs /= np.sum(probs, axis=1, keepdims=True)  #softmax denom = sum(e^(logit[j] + log(K))) where we sum for each example (row)
     index = np.arange(N),y  # From every row, choose y[i] column
-    li = -np.log(dlogits[index].clip(min=0.000001))
+    li = -np.log(probs[index].clip(min=1e-5))
     loss = np.sum(li) / N
+    dlogits = probs.copy()
     dlogits[index] -= 1     #probability error for each corresponding y[i] using one hot encoding (logits[i,y[i]]  1)
     dlogits /= N            #normalise gradient
 