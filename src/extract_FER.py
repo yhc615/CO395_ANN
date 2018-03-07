9218b11405f155f0ae2b3e317b929253010fc248
Yoon Hyuk Chang (Anno) <yoon.chang15@imperial.ac.uk>
2018-03-07 23:57:14 +0000

    pretty extract_FER

--- src/extract_FER.py
+++ src/extract_FER.py
@@ -1,24 +1,30 @@
 import matplotlib.image as mpimg
 import numpy as np
 
-X_train = []
-Y_train = []
-X_test = []
-Y_test = []
+def load_FER():#loads whole FER dataset, with the image paths for the X values
+	X_train = []
+	Y_train = []
+	X_test = []
+	Y_test = []
 
-with open("datasets/FER2013/labels_public.txt","r") as labels:
-	for line in labels:
-		img,emotion = line.split(",")
-		if(img.split("/")[0] == "Train"):
-			X_train.append(img)
-			Y_train.append(int(emotion.split("\n")[0]))
-		elif (img.split("/")[0] == "Test"):
-			X_test.append(img)
-			Y_test.append(int(emotion.split("\n")[0]))
-X_train1 = np.asarray(X_train)
-X_test1 = np.asarray(X_test)
-Y_train1 = np.asarray(Y_train)
-Y_test1 = np.asarray(Y_test)
+	with open("datasets/FER2013/labels_public.txt","r") as labels:
+		for line in labels:
+			img,emotion = line.split(",")
+			if(img.split("/")[0] == "Train"):
+				X_train.append(img)
+				Y_train.append(int(emotion.split("\n")[0]))
+			elif (img.split("/")[0] == "Test"):
+				X_test.append(img)
+				Y_test.append(int(emotion.split("\n")[0]))
+	return {
+	'X_train' : np.asarray(X_train),
+	'X_test' : np.asarray(X_test),
+	'Y_train' : np.asarray(Y_train),
+	'Y_test' : np.asarray(Y_test)
+	}
+
+if __name__ == '__main__':
+	print (load_FER())
+
+
 #img = mpimg.imread("datasets/FER2013/"+ XY_train[0][0])
-
-print(X_train1); print(X_test1);print(Y_train1); print(Y_test1)
