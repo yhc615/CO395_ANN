4e6e0a57246b700a2af17e0d47f7b3e563e339a7
smb00123 <shreyuss@hotmail.com>
2018-03-07 23:47:15 +0000

    extract FER numpy

--- src/extract_FER.py
+++ src/extract_FER.py
@@ -1,21 +1,24 @@
 import matplotlib.image as mpimg
 import numpy as np
 
-X_train = np.array([])
-Y_train = np.array([]) 
-X_test = np.array([])
-Y_test = np.array([])
+X_train = []
+Y_train = []
+X_test = []
+Y_test = []
 
 with open("datasets/FER2013/labels_public.txt","r") as labels:
 	for line in labels:
 		img,emotion = line.split(",")
 		if(img.split("/")[0] == "Train"):
-			X_train = np.append(X_train,img)
-			Y_train = np.append(Y_train, int(emotion.split("\n")[0]))
+			X_train.append(img)
+			Y_train.append(int(emotion.split("\n")[0]))
 		elif (img.split("/")[0] == "Test"):
-			X_test = np.append(X_test,img)
-			Y_test = np.append(Y_test, int(emotion.split("\n")[0]))
-print(X_test)
+			X_test.append(img)
+			Y_test.append(int(emotion.split("\n")[0]))
+X_train1 = np.asarray(X_train)
+X_test1 = np.asarray(X_test)
+Y_train1 = np.asarray(Y_train)
+Y_test1 = np.asarray(Y_test)
 #img = mpimg.imread("datasets/FER2013/"+ XY_train[0][0])
 
-#print(img)
+print(X_train1); print(X_test1);print(Y_train1); print(Y_test1)
