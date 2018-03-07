b63e2f7973ea3b331ff6a38392cf276e179f334f
smb00123 <shreyuss@hotmail.com>
2018-03-07 22:26:50 +0000

    extract_FER.py

--- src/extract_FER.py
+++ src/extract_FER.py
@@ -1,11 +1,12 @@
 X = []
 Y=[]
 
-with open("../datasets/FER2013/labels_public.txt","r") as labels:
+with open("datasets/FER2013/labels_public.txt","r") as labels:
 	i = 0
 	for line in labels:
 		img,emotion = line.split(",")
 		if i>0:
+			#if(img.split("/"))
 			X.append(img)
 			Y.append(emotion)
 			print(Y)
