6edc993079cdaa817f812eac72ed729308d5a258
smb00123 <shreyuss@hotmail.com>
2018-03-07 22:51:04 +0000

    extract_FER.py XY combined list

--- src/extract_FER.py
+++ src/extract_FER.py
@@ -1,14 +1,11 @@
-X_train, X_test = [],[]
-Y_train, Y_test = [],[]
+XY_train, XY_test = [],[]
 
 with open("datasets/FER2013/labels_public.txt","r") as labels:
 	for line in labels:
 		img,emotion = line.split(",")
 		if(img.split("/")[0] == "Train"):
-			X_train.append(img)
-			Y_train.append(int(emotion.split("\n")[0]))
+			XY_train.append((img,int(emotion.split("\n")[0])))
 		elif (img.split("/")[0] == "Test"):
-			X_test.append(img)
-			Y_test.append(int(emotion.split("\n")[0]))
-print(Y_train)
+			XY_test.append((img,int(emotion.split("\n")[0])))
+print(XY_train)
 
