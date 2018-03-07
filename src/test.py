1cf2c6fcfc695e4eae733c02b2d14eabf3d824f7
Yoon Hyuk Chang (Anno) <yoon.chang15@imperial.ac.uk>
2018-03-07 21:42:22 +0000

    skeleton functions for Q5, Q6

---
+++ src/test.py
@@ -0,0 +1,31 @@
+def test_fer_model(img_folder, model="/path/to/model"): #Q6
+    """
+    Given a folder with images, load the images (in lexico-graphical ordering
+    according to the file name of the images) and your best model to predict
+    the facial expression of each image.
+    Args:
+    - img_folder: Path to the images to be tested
+    Returns:
+    - preds: A numpy vector of size N with N being the number of images in
+    img_folder.
+    """
+    preds = None
+    ### Start your code here
+    ### End of code
+    return preds
+
+def test_deep_fer_model(img_folder, model="/path/to/model"): #Q5
+    """
+    Given a folder with images, load the images (in lexico-graphical ordering
+    according to the file name of the images) and your best model to predict
+    the facial expression of each image.
+    Args:
+    - img_folder: Path to the images to be tested
+    Returns:
+    - preds: A numpy vector of size N with N being the number of images in
+    img_folder.
+    """
+    preds = None
+    ### Start your code here
+    ### End of code
+    return preds
