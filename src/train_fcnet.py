9aa7e4e21c188231ceb01e5f6fb04e4c3652f771
Tom Miller <M1LL3RB0Y@users.noreply.github.com>
2018-03-07 23:00:41 +0000

    update models

--- src/train_fcnet.py
+++ src/train_fcnet.py
@@ -12,22 +12,22 @@ accuracy on the validation set.
 #                           BEGIN OF YOUR CODE                            #
 ###########################################################################
 
-out = get_CIFAR10_data(num_training=50)
+out = get_CIFAR10_data(num_training=25000)
 data = {
       'X_train': out['X_train'], # training data
       'y_train': out['y_train'], # training labels
       'X_val':  out['X_val'], # validation data
       'y_val': out['y_val'] # validation labels
     }
-model = FullyConnectedNet(hidden_dims=[200], num_classes=10, dropout=0, reg=0)
+model = FullyConnectedNet(hidden_dims=[100], num_classes=10, dropout=0, reg=0.5)
 solver = Solver(model, data,
                 update_rule='sgd',
                 optim_config={
                   'learning_rate': 2e-3,
                 },
                 lr_decay=0.95,
-                num_epochs=20, batch_size=2000,
-                print_every=10)
+                num_epochs=25, batch_size=250,
+                print_every=100)
 solver.train()
 
 ##############################################################################
