0e27607b404355f40732c7342036b249891f766a
smb00123 <shreyuss@hotmail.com>
2018-03-08 20:50:51 +0000

    test.py

--- src/test.py
+++ src/test.py
@@ -4,35 +4,35 @@ import matplotlib.image as mpimg
 import numpy as np
 
 def confusionMatrix(actual, predicted):
-  confusion = np.zeros(shape=(7,7)).astype(int)
-  for i,a in enumerate(actual):
-    confusion[actual[i]-1][predicted[i]-1] +=1
+    confusion = np.zeros(shape=(7,7)).astype(int)
+    for i,a in enumerate(actual):
+        confusion[actual[i]-1][predicted[i]-1] +=1
 
-  return confusion
+    return confusion
 
 def conMatStats(confusion):
-  recallrate, precisionrate,F1 = [0]*7,[0]*7,[0]*7
+    recallrate, precisionrate,F1 = [0]*7,[0]*7,[0]*7
   
-  TP,FP,TN,FN =[0]*7,[0]*7,[0]*7,[0]*7
+    TP,FP,TN,FN =[0]*7,[0]*7,[0]*7,[0]*7
   
-  for i in range(7):
-    for j in range(7):
-      TP[i] = confusion[i][i]
-      if j != i:
-        FN[i] += confusion[i][j]
+    for i in range(7):
+        for j in range(7):
+            TP[i] = confusion[i][i]
+            if j != i:
+                FN[i] += confusion[i][j]
         
-        FP[i] += confusion[j][i]
+            FP[i] += confusion[j][i]
         for k in range(7):
-          if k!=i:
-            TN[i] += confusion[j][k]
+            if k!=i:
+                TN[i] += confusion[j][k]
 
-  for i in range(7):
-    if(TP[i] != 0 or FN[i] !=0):
-      recallrate[i] = float(TP[i])/(TP[i]+FN[i])
-    if(TP[i] != 0 or FP[i] !=0):
-      precisionrate[i] = float(TP[i])/(TP[i]+FP[i])
-    if(precisionrate[i] !=0 or recallrate[i] != 0):
-      F1[i] = 2*(precisionrate[i]*recallrate[i])/(precisionrate[i]+recallrate[i])
+    for i in range(7):
+        if(TP[i] != 0 or FN[i] !=0):
+            recallrate[i] = float(TP[i])/(TP[i]+FN[i])
+        if(TP[i] != 0 or FP[i] !=0):
+            precisionrate[i] = float(TP[i])/(TP[i]+FP[i])
+        if(precisionrate[i] !=0 or recallrate[i] != 0):
+            F1[i] = 2*(precisionrate[i]*recallrate[i])/(precisionrate[i]+recallrate[i])
     classificationrate = 1-(1/np.sum(confusion))*(np.sum(confusion)-np.trace(confusion))
 
     return TP, FP, TN, FN, recallrate, precisionrate, F1, classificationrate
