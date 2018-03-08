e22b51b2be698cae02d04b3ba9b887841107fc08
Yoon Hyuk Chang (Anno) <yoon.chang15@imperial.ac.uk>
2018-03-08 21:36:52 +0000

    more fine tuning methods

--- src/testingA2.py
+++ src/testingA2.py
@@ -13,7 +13,7 @@ accuracy on the validation set.
 #                           BEGIN OF YOUR CODE                            #
 ###########################################################################
 #pickle_FER()
-
+optim = False # use for fine tuning learning rate
 out = get_FER(num_training=10000)
 data = {
       'X_train': out['X_train'], # training data
@@ -21,18 +21,43 @@ data = {
       'X_val':  out['X_val'], # validation data
       'y_val': out['y_val'] # validation labels
     }
-
 model = FullyConnectedNet(input_dim=48*48*1, hidden_dims=[40], num_classes=7, dropout=0, reg=0, seed=int(time.time()))
-solver = Solver(model, data,
-              update_rule='sgd_momentum',
-              optim_config={
-                'learning_rate': 1e-3,
-                'momentum': 0.5
-              },
-              lr_decay=0.8,
-              num_epochs=100, batch_size=100,
-              print_every=100)
-solver.train()
+
+if optim:
+  count = 1
+  reached = False
+  threshold = 0.35 #set threshold here
+  while not reached:
+    np.random.seed(int(time.time()))
+    print("iteration number{}".format(count))
+    lr = np.random.uniform(0.001,0.003)
+    print("testing lr: {}".format(lr))
+    solver = Solver(model, data,
+                    update_rule='sgd_momentum',
+                    optim_config={
+                      'learning_rate': lr,
+                      'momentum': 0.5
+                    },
+                    lr_decay=0.8,
+                    num_epochs=100, batch_size=100,
+                    print_every=100)
+    solver.train()
+    if max(solver.val_acc_history) >= threshold:
+      reached = True
+    count += 1
+
+  print("Final lr: {}".format(lr))
+else:
+  solver = Solver(model, data,
+                update_rule='sgd_momentum',
+                optim_config={
+                  'learning_rate': 1e-3,#0.0018742807840127864
+                  'momentum': 0.5
+                },
+                lr_decay=0.8,
+                num_epochs=100, batch_size=100,
+                print_every=100)
+  solver.train()
 
 pickle.dump(model, (open("./models/assignment2", "wb")))
 ##############################################################################
