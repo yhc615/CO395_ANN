8ebcc0ee9c09d777825bade1c4e875ff69703d41
Tom Miller <M1LL3RB0Y@users.noreply.github.com>
2018-03-08 17:47:26 +0000

    yh baby

---
+++ src/testingA2.py
@@ -0,0 +1,36 @@
+import numpy as np
+import time
+from src.fcnet import FullyConnectedNet
+from src.utils.solver import Solver
+from src.utils.data_utils import get_FER, pickle_FER
+
+"""
+TODO: Use a Solver instance to train a TwoLayerNet that achieves at least 50% 
+accuracy on the validation set.
+"""
+###########################################################################
+#                           BEGIN OF YOUR CODE                            #
+###########################################################################
+#pickle_FER()
+
+out = get_FER(num_training=10000)
+data = {
+      'X_train': out['X_train'], # training data
+      'y_train': out['y_train'], # training labels
+      'X_val':  out['X_val'], # validation data
+      'y_val': out['y_val'] # validation labels
+    }
+
+model = FullyConnectedNet(input_dim=48*48*1, hidden_dims=[40], num_classes=7, dropout=0, reg=0, seed=int(time.time()))
+solver = Solver(model, data,
+              update_rule='sgd_momentum',
+              optim_config={
+                'learning_rate': 1e-3
+              },
+              lr_decay=0.95,
+              num_epochs=250, batch_size=1000,
+              print_every=25)
+solver.train()
+##############################################################################
+#                             END OF YOUR CODE                               #
+##############################################################################
